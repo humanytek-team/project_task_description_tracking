48f3a89b00b4af733c007bb0ec751b77cfd5000c
mbrehumanytek <matias@humanytek.com>
2024-05-17 17:28:22 +0000

    quitar herencias

--- models/proyect_task.py
+++ models/proyect_task.py
@@ -2,6 +2,8 @@ from odoo import models, fields
 
 
 class ProjectTask(models.Model):
-    _inherit = ["project.task", "mail.thread", "mail.activity.mixin"]
+    _inherit = ["project.task"]
+    # no se hereda mail.threads ni mixing porque project.task ya los hereda al crearse
 
+    # tracking=True deberia funcionar
     description = fields.Html(tracking=True)
